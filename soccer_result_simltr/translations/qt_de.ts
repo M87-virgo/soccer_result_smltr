fc0e73f57d5cff01b4dfb9ab7b485d47cf19c33a
André Beyer <beyer341@onlinehome.de>
2020-11-03 19:16:46 +0100

    Add files via upload

---
+++ soccer_result_simltr/translations/qt_de.ts
@@ -0,0 +1,356 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="soccer_result_simltr_en_DE">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="mainwindow.ui" line="139"/>
+        <source>Database Connection</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="290"/>
+        <source>&lt;- Cont</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="20"/>
+        <source>Soccer Result Simulater 1.0.5-beta</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="52"/>
+        <source>Continents</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="78"/>
+        <source>Sub-Continents</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="94"/>
+        <location filename="mainwindow.ui" line="423"/>
+        <source>Nationalteams</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="164"/>
+        <source>Close</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="199"/>
+        <source>Selected Teams</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="212"/>
+        <source>------&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="225"/>
+        <source>&lt;-------</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="243"/>
+        <source>All teams
+ from left list-&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="257"/>
+        <source>Clear all teams
+from right list</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="276"/>
+        <source>All teams
+from continent -&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="322"/>
+        <source>Rounds</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="335"/>
+        <source>&lt;- All</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="351"/>
+        <source>Match-Schedule</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="364"/>
+        <source>Simulate
+Results</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="407"/>
+        <source>Result Table</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="430"/>
+        <source>Clubs</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="449"/>
+        <source>P</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="456"/>
+        <source>Points</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="463"/>
+        <source>G</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="470"/>
+        <source>Goals</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="477"/>
+        <source>GA</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="484"/>
+        <source>Goal Against</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="491"/>
+        <source>GD</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="498"/>
+        <source>Goal Difference</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="505"/>
+        <source>GP</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="512"/>
+        <source>Games Played</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="519"/>
+        <source>W</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="526"/>
+        <source>Win</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="533"/>
+        <source>D</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="540"/>
+        <source>Draw</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="547"/>
+        <source>L</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="554"/>
+        <source>Loose</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="561"/>
+        <source>LEGEND</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="577"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;br/&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="590"/>
+        <source>Continue</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="603"/>
+        <source>Back</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="647"/>
+        <source>Menü</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="657"/>
+        <source>toolBar</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="674"/>
+        <source>DB Editor</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="683"/>
+        <source>About</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>SqLiteDBEditor</name>
+    <message>
+        <location filename="sqlitedbeditor.ui" line="14"/>
+        <source>Dialog</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="sqlitedbeditor.ui" line="115"/>
+        <source>Club</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="sqlitedbeditor.ui" line="135"/>
+        <source>Sub-Continent</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="sqlitedbeditor.ui" line="155"/>
+        <source>Nationalteam</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="sqlitedbeditor.ui" line="291"/>
+        <source>Add League:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="sqlitedbeditor.ui" line="354"/>
+        <source>Add Club Rival:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="sqlitedbeditor.ui" line="408"/>
+        <source>League Name</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="sqlitedbeditor.ui" line="463"/>
+        <source>Date      Last Update:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="sqlitedbeditor.ui" line="517"/>
+        <source>Add Nationalteam Rival:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="sqlitedbeditor.ui" line="537"/>
+        <location filename="sqlitedbeditor.ui" line="838"/>
+        <source>Rival</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="sqlitedbeditor.ui" line="590"/>
+        <source>Close</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="sqlitedbeditor.ui" line="708"/>
+        <source>Add Club:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="sqlitedbeditor.ui" line="728"/>
+        <source>Continent</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="sqlitedbeditor.ui" line="818"/>
+        <source>Tables</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="sqlitedbeditor.ui" line="858"/>
+        <source>Nations</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="sqlitedbeditor.ui" line="878"/>
+        <source>FIFA CODE</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="sqlitedbeditor.ui" line="972"/>
+        <source>Search</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="sqlitedbeditor.ui" line="1009"/>
+        <source>Date Last Update</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="sqlitedbeditor.ui" line="1037"/>
+        <source>Update</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="sqlitedbeditor.ui" line="1075"/>
+        <source>filter by 
+continents</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="sqlitedbeditor.ui" line="1104"/>
+        <source>filter by 
+sub-continents</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="sqlitedbeditor.ui" line="1235"/>
+        <source>filter by
+ leagues</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="sqlitedbeditor.ui" line="1264"/>
+        <source>show all
+clubs</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="sqlitedbeditor.ui" line="1293"/>
+        <source>show all
+nations</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
